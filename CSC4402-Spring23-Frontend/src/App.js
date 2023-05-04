42be3a5e4e023fb378a4a0b99e30aa13a0733fe4
nightmeow <xxdsheep@gmail.com>
2023-05-04 10:38:40 -0500

    format frontend

--- CSC4402-Spring23-Frontend/src/App.js
+++ CSC4402-Spring23-Frontend/src/App.js
@@ -2,18 +2,43 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LoadTablePage from "./pages/LoadTablePage";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 function App() {
+
+	let component
+
+	switch (window.location.pathname){
+		case"/":
+		component = <Home />
+		break
+		case "/LoadBoard":
+		component = <LoadTablePage />
+		break
+		case "/Login":
+		component = <Login />
+		break
+		case "/Profile":
+		component = <Profile />
+		break
+	}
 	return (
 		<>
-			<div className="page">
-				<Router>
-					<Routes>
-						<Route index element={<Home />} />\
-						<Route path="/LoadBoard" element={<LoadTablePage />} />
-					</Routes>
-				</Router>
+			<Navbar />
+			<div className="App">
+				{component}
 			</div>
+
+			{/*<div className="page">*/}
+			{/*	<Router>*/}
+			{/*		<Routes>*/}
+			{/*			<Route index element={<Home />} />\*/}
+			{/*			<Route path="/LoadBoard" element={<LoadTablePage />} />*/}
+			{/*		</Routes>*/}
+			{/*	</Router>*/}
+			{/*</div>*/}
 		</>
 	);
 }
